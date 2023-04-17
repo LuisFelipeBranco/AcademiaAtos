b8f8953fa77a87deccee0393a6d68e95b64cb5ad
LuisFelipeBranco <bpl_@outlook.com>
2023-04-17 16:58:00 -0300

    'Sincronizando'

---
+++ exercicios/Matrizes/01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01
+{//1) Crie uma Matriz 5x3. Na primeira coluna, solicite que o usuário preencha. A 2ª coluna, some 10 aos elementos da 1ª coluna. Na 3º coluna, armazene o dobro dos elementos da 1ª coluna.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5, 3];
+
+            Console.WriteLine("Preencha a primeira coluna da matriz:");
+
+            for (int linha = 0; linha < 5; linha++)
+            {
+                Console.Write($"Elemento {linha + 1}: ");
+                matriz[linha, 0] = int.Parse(Console.ReadLine());
+            }
+
+            for (int linha = 0; linha < 5; linha++)
+            {
+                matriz[linha, 1] = matriz[linha, 0] + 10;
+                matriz[linha, 2] = matriz[linha, 0] * 2;
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Matriz resultante:");
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.Write(matriz[linha, coluna] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ exercicios/Matrizes/02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics;
+
+namespace _02
+{/*
+  2) Solicite ao usuário, preencher uma Matriz 3x3
+
+Informe ao usuário:
+*A soma dos elementos de cada linha
+	-Ex: Linha 1: 30
+	     Linha 2: 17
+*A soma dos elementos de cada coluna
+	-Ex: Coluna 1: 23
+	     Coluna 2: 36
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            int cont = 0;
+            
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) {
+
+                    Console.WriteLine($"Informe o elemento que preencherá a linha {i+1} coluna {j}: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Matriz resultante: ");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{matriz[i, 0]} - {matriz[i, 1]} - {matriz[i, 2]}");
+            }
+
+            Console.WriteLine("Soma das linhas:");
+
+            for (int i = 0; i < 3; i++) {
+                cont = 0;
+                for (int j = 0; j<3; j++) {
+                    cont += matriz[i, j];
+                }
+                Console.WriteLine($"Soma da linha {i+1} e igual a: {cont}");
+            }
+            Console.WriteLine("________________________________________");
+
+            Console.WriteLine("Soma das colunas:");
+            for (int i = 0; i<3; i++) {
+                cont = 0;
+                for (int j = 0; j < 3; j++) {
+                    cont += matriz[j,i];
+                }
+                Console.WriteLine($"Soma da coluna {i} e igual a: {cont}");
+            }
+
+        }
+    }
+}
