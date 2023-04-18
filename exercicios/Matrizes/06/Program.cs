db6e91478ff9491266929e61fb48949b39f72dc0
LuisFelipeBranco <bpl_@outlook.com>
2023-04-18 06:44:36 -0300

    Exercicios 07 e 08

---
+++ exercicios/Matrizes/06/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _06
+{/*
+  6) Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-9. Após isso determine o maior número da matriz.
+Random random = new Random();
+int randomNumber = random.Next(0, 100);
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int linha = 2, coluna = 3, maior = 0;
+            int[,] matriz = new int[linha, coluna];
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = random.Next(0, 100); //gera um número aleatório entre 1 e 10
+                }
+            }
+            
+            Console.WriteLine("Matriz desenhada:");
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.WriteLine($"{matriz[i, 0]} - {matriz[i, 1]} - {matriz[i, 2]}");
+            }
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    if (matriz[i,j] > maior) { 
+                        maior = matriz[i, j];
+                    }
+                }
+            }
+            Console.WriteLine($"O maior elemento da matriz é: {maior}");
+        }
+    }
+}
---
+++ exercicios/Matrizes/07/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _07
+{//7) Leia duas matrizes A e B com 3x3 elementos. Construir uma matriz C, onde cada elemento de C é a subtração do elemento correspondente de A com B.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int linha = 3, coluna = 3;
+            double[,] matriz = new double[linha, coluna];
+            double[,] matriz2 = new double[linha, coluna];
+            double[,] matriz3 = new double[linha, coluna];
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = random.Next(0, 10); //gera um número aleatório entre 1 e 10
+                }
+            }
+
+            for (int i = 0; i < matriz2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz2.GetLength(1); j++)
+                {
+                    matriz2[i, j] = random.Next(0, 10); //gera um número aleatório entre 1 e 10
+                }
+            }
+
+            Console.WriteLine("Matriz 1 desenhada: ");
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.WriteLine($"{matriz[i, 0]} - {matriz[i, 1]} - {matriz[i, 2]}");
+            }
+
+            Console.WriteLine("Matriz 2 desenhada: ");
+            for (int i = 0; i < matriz2.GetLength(0); i++)
+            {
+                Console.WriteLine($"{matriz2[i, 0]} - {matriz2[i, 1]} - {matriz2[i, 2]}");
+            }
+
+            Console.WriteLine("Subtração das 2 matrizes: ");
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz3[i, j] = matriz[i,j] - matriz2[i, j];
+                }
+            }
+            
+            for (int i = 0; i < matriz3.GetLength(0); i++)
+            {
+                Console.WriteLine($"{matriz3[i, 0]} - {matriz3[i, 1]} - {matriz3[i, 2]}");
+            }
+        }
+    }
+}
